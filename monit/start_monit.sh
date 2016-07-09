d72e7852edf44241be39368a92f89c2765588da0
Raja. Soundaramourty <rajasoun@cisco.com>
2016-07-09 07:49:23 -0400

    Monit Config

--- monit/start_monit.sh
+++ monit/start_monit.sh
@@ -1,1 +1,1 @@
-docker run --name="monit" --restart=always -d -p 12812:2812 --privileged -v /var/run/docker.sock:/var/run/docker.sock rajasoun/monit:latest
+docker run --name="monit" --restart=always -d -p 2812:2812 --privileged -v /var/run/docker.sock:/var/run/docker.sock rajasoun/monit:v1
